bb65959fb055ccbdcd33edbfa5782e1119769709
Steelnox <eric14pastor@gmail.com>
2021-01-08 09:30:39 +0100

    Clase videojuego creada

---
+++ UD10_Ej5/UD10_Ej5/Videojuego.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD10_Ej5
+{
+    public class Videojuego
+    {
+        private string titulo;
+        private float horas_est;
+        private bool entregado;
+        private string genero;
+        private string compañia;
+
+        public Videojuego()
+        {
+            titulo = "";
+            horas_est = 10;
+            entregado = false;
+            genero = "";
+            compañia = "";
+        }
+
+        public Videojuego(string titulo, float horas_est)
+        {
+            this.titulo = titulo;
+            this.horas_est = horas_est;
+            entregado = false;
+            genero = "";
+            compañia = "";
+        }
+
+        public Videojuego(string titulo, float horas_est, string genero, string compañia) 
+        {
+            this.genero = genero;
+            this.compañia = compañia;
+            this.titulo = titulo;
+            this.horas_est = horas_est;
+            entregado = false;
+        }
+
+        public string Titulo { get => titulo; set => titulo = value; }
+        public float Horas_est { get => horas_est; set => horas_est = value; }
+        public string Genero { get => genero; set => genero = value; }
+        public string Compañia { get => compañia; set => compañia = value; }
+
+        public override string ToString()
+        {
+            return titulo.ToString() + ": " + horas_est.ToString() + ": " + genero.ToString() + ": " + compañia.ToString() + ": " + entregado.ToString();
+        }
+    }
+}
