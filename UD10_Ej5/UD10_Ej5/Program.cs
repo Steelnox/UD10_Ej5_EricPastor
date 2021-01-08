4b06ba5e43febc5da22a74b64befacfc6839a685
Steelnox <eric14pastor@gmail.com>
2021-01-08 09:22:10 +0100

    Clase Serie creada

---
+++ UD10_Ej5/UD10_Ej5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UD10_Ej5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UD10_Ej5/UD10_Ej5/Serie.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD10_Ej5
+{
+    public class Serie
+    {
+        private string titulo;
+        private int ntemporadas;
+        private bool entregado;
+        private string genero;
+        private string creador;
+
+        public Serie()
+        {
+            titulo = "";
+            ntemporadas = 3;
+            entregado = false;
+            genero = "";
+            creador = "";
+        }
+
+        public Serie(string titulo, string creador)
+        {
+            this.titulo = titulo;
+            this.creador = creador;
+            ntemporadas = 3;
+            entregado = false;
+            genero = "";
+        }
+
+        public Serie(string titulo, int ntemporadas, string genero, string creador)
+        {
+            this.titulo = titulo;
+            this.ntemporadas = ntemporadas;
+            this.genero = genero;
+            this.creador = creador;
+            entregado = false;
+        }
+
+        public string Titulo { get => titulo; set => titulo = value; }
+        public int Ntemporadas { get => ntemporadas; set => ntemporadas = value; }
+        public string Genero { get => genero; set => genero = value; }
+        public string Creador { get => creador; set => creador = value; }
+
+        public override string ToString()
+        {
+            return titulo.ToString() + ": " + ntemporadas.ToString() + ": " + genero.ToString() + ": " + creador.ToString() + ": " + entregado.ToString() ;
+        }
+    }
+}
